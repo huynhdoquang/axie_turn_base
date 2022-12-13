f3f3486112e03b0e8269010b68c81f869d9eeac6
huynh_do <huynh.fetel@gmail.com>
2022-12-13 22:47:19 +0700

    update for webGL build

--- Assets/Scripts/SelectFileControl.cs
+++ Assets/Scripts/SelectFileControl.cs
@@ -1,6 +1,7 @@
 using SFB;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
